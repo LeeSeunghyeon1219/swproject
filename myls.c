4734f5eb643143d037a386dbc1d90182d17ed708
Seunghyeon Lee <5644718@naver.com>
2018-11-20 06:34:21 -0800

    add .gitignore and modified myls.c

--- myls.c
+++ myls.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-
+#include <string.h>
 int main(int argc, char *argv[])
 {
 	struct stat sbuf;
@@ -31,20 +31,26 @@ int main(int argc, char *argv[])
 		}
 		else
 		{
-			printf("%s	",dp->d_name);	
+			if(strcmp(dp->d_name,".")==0)
+				continue;
+			if(strcmp(dp->d_name,"..")==0)
+				continue;
+			if(strcmp(dp->d_name,".git")==0)
+				continue;
+			printf("%s\n",dp->d_name);	
 			stat(dp->d_name, &sbuf);
-			if(S_ISREG(sbuf.st_mode))
-			{
-				printf("regular file\n");
-			}
-			else if(S_ISDIR(sbuf.st_mode))
-			{
-				printf("Directory\n");
-			}
-			else
-			{
-				printf("Other\n");
-			}
+	//		if(S_ISREG(sbuf.st_mode))
+	//		{
+	//			printf("regular file\n");
+	//		}
+	//		else if(S_ISDIR(sbuf.st_mode))
+	//		{
+	//			printf("Directory\n");
+	//		}
+	//		else
+	//		{
+	//			printf("Other\n");
+	//		}
 		}
 	}
 	
