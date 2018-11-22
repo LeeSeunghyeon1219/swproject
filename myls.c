0f67bd9913f97fed10cb08182aee0e2ea2ae8dc8
Seunghyeon Lee <5644718@naver.com>
2018-11-22 04:57:41 -0800

    add ls option -a --all

--- myls.c
+++ myls.c
@@ -7,52 +7,83 @@
 #include <string.h>
 int main(int argc, char *argv[])
 {
-	struct stat sbuf;
-	DIR *dirp;
-	struct dirent *dp;
-	if (argc != 2)
-	{
-		fprintf(stderr,"usage myls dirname\n");
-		exit(0);
-	}
-	
-	dirp=opendir(argv[1]);
-	if(dirp==NULL)
-	{
-		perror("opendir");
-		exit(0);
-	}
-	for(;;)
-	{
-		dp=readdir(dirp);
-		if(dp==NULL)
-		{
-			break;
-		}
-		else
-		{
-			if(strcmp(dp->d_name,".")==0)
-				continue;
-			if(strcmp(dp->d_name,"..")==0)
-				continue;
-			if(strcmp(dp->d_name,".git")==0)
-				continue;
-			printf("%s\n",dp->d_name);	
-			stat(dp->d_name, &sbuf);
-	//		if(S_ISREG(sbuf.st_mode))
-	//		{
-	//			printf("regular file\n");
-	//		}
-	//		else if(S_ISDIR(sbuf.st_mode))
-	//		{
-	//			printf("Directory\n");
-	//		}
-	//		else
-	//		{
-	//			printf("Other\n");
-	//		}
-		}
-	}
-	
-		
-}
+ int num=0;
+ struct stat sbuf;
+ DIR *dirp;
+ struct dirent *dp;
+ 
+ //compare argc
+ if (argc != 1 & argc!=2)
+ {
+  fprintf(stderr,"usage myls dirname\n");
+  exit(0);
+ }
+ else if(argc ==2)
+ {
+  if(strcmp(argv[1],"..")==0)
+   dirp=opendir("..");
+  else
+   dirp=opendir(".");
+  
+  if(strcmp(argv[1],"-a")==0|strcmp(argv[1],"--all")==0)
+   num=1;
+ }
+ else
+  dirp=opendir(".");
+
+
+ if(num==0)//base
+ {
+  if(dirp==NULL)
+  {
+   perror("opendir");
+   exit(0);
+  }
+  else
+  {
+   for(;;)
+   {
+    dp=readdir(dirp);
+    if(dp==NULL)
+     break;
+    else
+    {
+     if(strcmp(dp->d_name,".")==0)
+      continue;
+     if(strcmp(dp->d_name,"..")==0)
+       continue;
+     if(strcmp(dp->d_name,".git")==0)
+      continue;
+     if(strcmp(dp->d_name,".gitignore")==0)
+      continue;
+     printf("%s  ",dp->d_name);	
+     stat(dp->d_name, &sbuf);
+    }
+   }
+   printf("\n");  
+  }
+ }
+ else if(num==1)//-a --all
+ {
+  if(dirp==NULL)
+  {
+   perror("opendir");
+   exit(0);
+  }
+  else
+  {
+   for(;;)
+   {
+    dp=readdir(dirp);
+    if(dp==NULL)
+     break;
+    else
+    {
+     printf("%s  ",dp->d_name);
+     stat(dp->d_name, &sbuf);
+    }
+   }
+  }
+  printf("\n");
+ }
+} 
