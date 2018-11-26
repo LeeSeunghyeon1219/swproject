2092f11db51d8c7e350add23d833821fe63f93e3
Seunghyeon Lee <5644718@naver.com>
2018-11-26 01:35:50 -0800

    add ls

--- myls.c
+++ myls.c
@@ -5,6 +5,11 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <grp.h>
+
+
+
+
 int main(int argc, char *argv[])
 {
 	int num=0;
@@ -90,6 +95,51 @@ int main(int argc, char *argv[])
  	}
 	else if(num==2)//-l
 	{
-		printf("-l\n");
+		if(dirp=NULL)
+		{
+			perror("opendir");
+			exit(0);
+		}
+		else
+		{
+			for(;;)
+			{
+				dp=readdir(dirp);
+				if(dp==NULL)
+					break;
+				else
+				{
+					mode_t file_mode;
+					struct tm *tminfo;
+					struct passwd *my_passwd;
+					struct group *my_group;
+					char buf1[80];
+					if(stat(dp->d_name,&sbuf)==-1)
+					{
+						perror("Error");
+						exit(0);
+					}
+					file_mode=sbuf.st_mode;
+					//rwx(file_mode);
+					printf("%d ",sbuf.st_nlink);
+					my_passwd=getpwuid(sbuf.st_uid);
+					my_group=getgrgid(sbuf.st_gid);
+					printf("%s ",my_passwd->pw_name);
+					printf("%s ",my_group->gr_name);
+					printf("%d ",file_info.st_size);
+					
+					tminfo=localtime((&sbuf.st_atime));
+					strftime(buf1,80,"%m/%d %H:%M",tminfo);
+					printf("%s ",buf1);
+					
+					if(dp->d_name !=0)
+						printf("%s\n",dp->d_name);
+					
+				}
+			}
+						
+		}
 	}
 } 
+
+
