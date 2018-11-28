c2e135edd84636aee204dc51323fcd853fd825fd
Seunghyeon Lee <5644718@naver.com>
2018-11-27 22:09:37 -0800

    ls -l modified

--- myls.c
+++ myls.c
@@ -1,23 +1,19 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <dirent.h>
-#include<time.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
 #include <grp.h>
-
-
-
-
 int main(int argc, char *argv[])
 {
 	int num=0;
 	struct stat sbuf;
 	DIR *dirp;
 	struct dirent *dp;
- 
+
 	 //compare argc
 	if (argc != 1 & argc!=2)
 	{
@@ -25,12 +21,12 @@ int main(int argc, char *argv[])
 		exit(0);
 	}
 	else if(argc ==2)
-	{	
+	{
 		if(strcmp(argv[1],"..")==0)
 			dirp=opendir("..");
  		else
 			dirp=opendir(".");
-  
+
   		if(strcmp(argv[1],"-a")==0|strcmp(argv[1],"--all")==0)
    			num=1;
 		else if(strcmp(argv[1],"-l")==0)
@@ -94,9 +90,10 @@ int main(int argc, char *argv[])
   		}
   		printf("\n");
  	}
-	else if(num==2)//-l
+	else //-l
 	{
-		if(dirp=NULL)
+		
+		if(dirp==NULL)
 		{
 			perror("opendir");
 			exit(0);
@@ -110,28 +107,28 @@ int main(int argc, char *argv[])
 					break;
 				else
 				{
+                                        if(strcmp(dp->d_name,".")==0)
+                                                continue;
+                                        if(strcmp(dp->d_name,"..")==0)
+                                                continue;
+
 					mode_t file_mode;
 					struct tm *tminfo;
-					//struct passwd *my_passwd;
-					//struct group *my_group;
+					struct passwd *my_passwd;
+					struct group *my_group;
 					char buf1[80];
-					if(stat(dp->d_name,&sbuf)==-1)
-					{
-						perror("Error");
-						exit(0);
-					}
-					//file_mode=sbuf.st_mode;
+					stat(dp->d_name,&sbuf);
+					file_mode=sbuf.st_mode;
 					//rwx(file_mode);
-					//printf("%d ",(int)sbuf.st_nlink);
-					//my_passwd=getpwuid(sbuf.st_uid);
-					//my_group=getgrgid(sbuf.st_gid);
-					//printf("%d ",sbuf.st_uid);
-					//printf("%d ",sbuf.st_gid);
-					//printf("%d ",(int)sbuf.st_size);
-					
-					//tminfo=localtime((&sbuf.st_atime));
-					//strftime(buf1,80,"%m/%d %H:%M",tminfo);
-					//printf("%s ",buf1);
+					printf("%d ",(int)sbuf.st_nlink);
+			//		my_passwd=getpwuid(sbuf.st_uid);
+			//		my_group=getgrgid(sbuf.st_gid);
+				//	printf("%s ",sbuf.st_uid);
+				//	printf("%s ",sbuf.st_gid);
+					printf("%d ",(int)sbuf.st_size);
+					tminfo=localtime((&sbuf.st_atime));
+					strftime(buf1,80,"%m/%d %H:%M",tminfo);
+					printf("%s ",buf1);
 					
 					if(dp->d_name !=0)
 						printf("%s\n",dp->d_name);
