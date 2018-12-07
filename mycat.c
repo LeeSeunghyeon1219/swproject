e1e22ba289d8d82bfa5b0593299d39327055bac7
Seunghyeon Lee <5644718@naver.com>
2018-12-06 20:43:29 -0800

    modified mycat

--- mycat.c
+++ mycat.c
@@ -1,15 +1,15 @@
 #include <fcntl.h>
 #include <sys/types.h>
- #include<sys/stat.h>
- #include <dirent.h>
- #include <time.h>
- #include <stdio.h>
- #include <stdlib.h>
- #include <pwd.h>
- #include <unistd.h>
- #include <string.h>
- #include <grp.h>
- #define SZ_BUF 1024
+#include<sys/stat.h>
+#include <dirent.h>
+#include <time.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pwd.h>
+#include <unistd.h>
+#include <string.h>
+#include <grp.h>
+#define SZ_BUF 1024
 int main(int argc, char * argv[])
 {
 	int fd_in,fr;
@@ -20,40 +20,37 @@ int main(int argc, char * argv[])
 	mode_t file_mode;
 	int num=0;
 	if(argc ==1)
-	{
-		fprintf(stderr,"usage mycat dirname\n");
-		exit(0);
-	}
-	// file check 
-	dirp=opendir(".");
-	for(int i=1;i<argc;i++)
-	{
-		  int s=stat(argv[i],&sbuf);
-		  if(s==-1)
-		  {
-			num=1;
-			break;
-		  }
-                  file_mode=sbuf.st_mode;
-                  if(S_ISDIR(file_mode))
-		  {
-			num=2;
-			break;
-		  }
-	}
-	if(num==1)
+ 	{
+ 		fprintf(stderr,"usage mycat dirname\n");
+ 		exit(0);
+ 	}
+ 	// file check 
+ 	dirp=opendir(".");
+ 	for(int i=1;i<argc;i++)
+ 	{
+ 		  int s=stat(argv[i],&sbuf);
+ 		  if(s==-1)
+ 		  {
+ 			num=1;
+ 			break;
+ 		  }
+                   file_mode=sbuf.st_mode;
+                   if(S_ISDIR(file_mode))
+ 		  {
+ 			num=2;
+ 			break;
+ 		  }
+ 	}
+ 	if(num==1)
 		printf("There is no file in here!\n");
 	else if(num==2)
 		printf("Directory name!\n");
-	//print 
+	//open file and write terminal
 	for(int i=1;i<argc;i++)
 	{
-		fd_in=open(argv[1], O_RDONLY);
-		while(fr=read(0,buf,SZ_BUF))
-		{
-			write(1,buf,fr);
-			memset(buf,0,sizeof(buf));
-		}
+		FILE * fd=fopen(argv[i],"rt");
+		while(fgets(buf,SZ_BUF,fd)!=NULL)
+			printf("%s",buf);
 	}
 			
 }
