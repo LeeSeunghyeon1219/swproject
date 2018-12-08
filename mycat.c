afb9b26240042b030118673578f34dbb3bb8e665
Seunghyeon Lee <5644718@naver.com>
2018-12-08 04:46:15 -0800

    function mycat

--- mycat.c
+++ mycat.c
@@ -10,6 +10,51 @@
 #include <string.h>
 #include <grp.h>
 #define SZ_BUF 1024
+void mycat(int argc, char *argv[])
+{
+        int fd_in,fr;
+        struct stat sbuf;
+        DIR *dirp;
+        struct dirent *dp;
+        char buf[SZ_BUF];
+        mode_t file_mode;
+        int num=0;
+
+        dirp=opendir(".");
+        for(int i=1;i<argc;i++)
+        {
+                  int s=stat(argv[i],&sbuf);
+                  if(s==-1)
+                  {
+                        num=1;
+                        break;
+                  }
+                   file_mode=sbuf.st_mode;
+                   if(S_ISDIR(file_mode))
+                  {
+                        num=2;
+                        break;
+                  }
+        }
+        if(num==1)
+        {
+                printf("There is no file in here!\n");
+                return;
+        }
+        else if(num==2)
+        {
+	        printf("Directory name!\n");
+		return;	
+	}        
+	//open file and write terminal
+        for(int i=1;i<argc;i++)
+        {
+                FILE * fd=fopen(argv[i],"rt");
+                while(fgets(buf,SZ_BUF,fd)!=NULL)
+                        printf("%s",buf);
+        }
+	return;
+}
 int main(int argc, char * argv[])
 {
 	int fd_in,fr;
@@ -19,39 +64,11 @@ int main(int argc, char * argv[])
 	char buf[SZ_BUF];
 	mode_t file_mode;
 	int num=0;
-	if(argc ==1)
- 	{
- 		fprintf(stderr,"usage mycat dirname\n");
+	if(argc==1)
+	{
+		fprintf(stderr,"usage mycat dirname\n");
  		exit(0);
  	}
- 	// file check 
- 	dirp=opendir(".");
- 	for(int i=1;i<argc;i++)
- 	{
- 		  int s=stat(argv[i],&sbuf);
- 		  if(s==-1)
- 		  {
- 			num=1;
- 			break;
- 		  }
-                   file_mode=sbuf.st_mode;
-                   if(S_ISDIR(file_mode))
- 		  {
- 			num=2;
- 			break;
- 		  }
- 	}
- 	if(num==1)
-		printf("There is no file in here!\n");
-	else if(num==2)
-		printf("Directory name!\n");
-
-	//open file and write terminal
-	for(int i=1;i<argc;i++)
-	{
-		FILE * fd=fopen(argv[i],"rt");
-		while(fgets(buf,SZ_BUF,fd)!=NULL)
-			printf("%s",buf);
-	}
-			
+	else
+		mycat(argc, argv);
 }
